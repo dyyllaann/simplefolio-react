4cb750a2f788318d85c3ce16a87d66c0c3726fd5
dyyllaann <dylan.arveson@gmail.com>
2023-04-26 22:59:32 -0700

    Add socials to footer.

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -8,17 +8,16 @@ const Footer = () => {
   };
 
   return (
-    <div className="footer px-8 py-16 lg:py-32 flex justify-center align-center flex-col bg-neutral-800">
-      <h1 className="uppercase font-bold text-center text-white text-bold text-4xl">Footer</h1>
-      <div>
+    <div className="footer px-8 py-16 flex justify-center align-center flex-col bg-neutral-800">
+      <div className="mx-auto text-3xl text-neutral-400 mb-8 space-x-10">
         <a rel="noreferrer" href={contact.twitter} target="_blank">
-          <i className="fa fa-twitter fa-inverse"></i>
+          <i className="devicon-twitter-original"></i>
         </a>
         <a rel="noreferrer" href={contact.linkedin} target="_blank">
-          <i className="fa fa-linkedin fa-inverse"></i>
+          <i className="devicon-linkedin-plain"></i>
         </a>
         <a rel="noreferrer" href={contact.github} target="_blank">
-          <i className="fa fa-github fa-inverse"></i>
+          <i className="devicon-github-original"></i>
         </a>
       </div>
       <span className="text-sm text-center text-neutral-600">
