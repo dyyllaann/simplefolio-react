5185a098bc543c968d6eff597cf4090dedfbd80b
arvesdj <arvesdj@cs.washington.edu>
2026-08-04 23:10:40 -0700

    Separate styles into dedicated style sheets.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+import '@/styles/themes.css'
 import '@/styles/globals.css'
+import '@/styles/components.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
